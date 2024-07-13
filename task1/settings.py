76893e4156ea15d16b5f600fbc785c28c82bfba5
Gehendra1 <chaudharygehendra46@gmail.com>
2024-07-13 09:59:42 +0545

    last commit

--- task1/settings.py
+++ task1/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'task1.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'hospital',
+        'NAME': 'Hospital',
         'USER' : 'root',
         'PASSWORD': '',
         'HOST': '127.0.0.1',
